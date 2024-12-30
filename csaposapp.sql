72925acf2010aafb1be51d8c47443e8b8a1fddc3
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2024-12-30 04:57:49 +0100

    Refactored database

--- csaposapp.sql
+++ csaposapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2024 at 05:37 AM
+-- Generation Time: Dec 30, 2024 at 04:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,13 +31,20 @@ USE `csaposapp`;
 
 CREATE TABLE `business_hours` (
   `id` char(36) NOT NULL,
-  `monday` time NOT NULL,
-  `tuesday` time NOT NULL,
-  `wednesday` time NOT NULL,
-  `thursday` time NOT NULL,
-  `friday` time NOT NULL,
-  `saturday` time NOT NULL,
-  `sunday` time NOT NULL,
+  `monday_open` time NOT NULL,
+  `tuesday_open` time NOT NULL,
+  `wednesday_open` time NOT NULL,
+  `thursday_open` time NOT NULL,
+  `friday_open` time NOT NULL,
+  `saturday_open` time NOT NULL,
+  `sunday_open` time NOT NULL,
+  `monday_close` time NOT NULL,
+  `tuesday_close` time NOT NULL,
+  `wednesday_close` time NOT NULL,
+  `thursday_close` time NOT NULL,
+  `friday_close` time NOT NULL,
+  `saturday_close` time NOT NULL,
+  `sunday_close` time NOT NULL,
   `location_id` char(36) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -52,6 +59,7 @@ CREATE TABLE `events` (
   `id` char(36) NOT NULL,
   `location_id` char(36) DEFAULT NULL,
   `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `timefrom` datetime NOT NULL,
   `timeto` datetime NOT NULL,
   `img_url` varchar(255) DEFAULT NULL
@@ -78,6 +86,7 @@ CREATE TABLE `event_attendance` (
 CREATE TABLE `locations` (
   `id` char(36) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `capacity` int(11) NOT NULL,
   `number_of_tables` int(11) NOT NULL,
   `rating` tinyint(4) DEFAULT NULL,
@@ -91,11 +100,11 @@ CREATE TABLE `locations` (
 -- Dumping data for table `locations`
 --
 
-INSERT INTO `locations` (`id`, `name`, `capacity`, `number_of_tables`, `rating`, `is_highlighted`, `is_open`, `created_at`, `img_url`) VALUES
-('5ecdf8ff-8bf5-4cc1-98eb-c8af87faf4cd', 'City Pub', 80, 16, -1, 0, 1, '2024-12-26 03:20:59', 'string'),
-('9c458b82-7eb9-4fc9-a198-784245d13425', 'Sörpatika', 60, 12, -1, 0, 1, '2024-12-26 03:21:10', 'string'),
-('c6276e02-67f8-45e1-ba7a-6271a93bea02', 'Alt lány hely', 20, 4, -1, 0, 1, '2024-12-26 03:21:45', 'string'),
-('cbdd928f-5abb-4a0b-9023-0866107cfa9a', 'Félidő Söröző', 40, 8, -1, 0, 1, '2024-12-26 03:20:38', 'string');
+INSERT INTO `locations` (`id`, `name`, `description`, `capacity`, `number_of_tables`, `rating`, `is_highlighted`, `is_open`, `created_at`, `img_url`) VALUES
+('5ecdf8ff-8bf5-4cc1-98eb-c8af87faf4cd', 'City Pub', '', 80, 16, -1, 0, 1, '2024-12-26 03:20:59', 'string'),
+('9c458b82-7eb9-4fc9-a198-784245d13425', 'Sörpatika', '', 60, 12, -1, 0, 1, '2024-12-26 03:21:10', 'string'),
+('c6276e02-67f8-45e1-ba7a-6271a93bea02', 'Alt lány hely', '', 20, 4, -1, 0, 1, '2024-12-26 03:21:45', 'string'),
+('cbdd928f-5abb-4a0b-9023-0866107cfa9a', 'Félidő Söröző', '', 40, 8, -1, 0, 1, '2024-12-26 03:20:38', 'string');
 
 -- --------------------------------------------------------
 
@@ -107,6 +116,7 @@ CREATE TABLE `orders` (
   `id` char(36) NOT NULL,
   `user_id` char(36) NOT NULL,
   `table_id` char(36) NOT NULL,
+  `location_id` char(36) NOT NULL,
   `order_status` enum('pending','accepted','completed','paid','rejected') DEFAULT 'pending',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -137,6 +147,7 @@ CREATE TABLE `products` (
   `name` varchar(100) NOT NULL,
   `category` varchar(50) NOT NULL,
   `price` int(11) NOT NULL,
+  `discount_percentage` int(11) NOT NULL DEFAULT 0,
   `stock_quantity` int(11) DEFAULT 0,
   `is_active` tinyint(1) DEFAULT 0,
   `img_url` varchar(255) DEFAULT NULL
@@ -146,10 +157,10 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `category`, `price`, `stock_quantity`, `is_active`, `img_url`) VALUES
-('875695ab-a760-486a-a801-9b052c8738b7', 'Ez egy teszt termék', 'drink', 1000, 10, 1, 'drink/875695ab-a760-486a-a801-9b052c8738b7.webp'),
-('ac107756-f63c-4538-b4cc-91fc0cad6d82', 'Finom ital', 'drink', 2660, 24, 1, 'drink/ac107756-f63c-4538-b4cc-91fc0cad6d82.webp'),
-('c5d2b92e-c78f-4bf0-8504-154f033a8898', 'Finom burger', 'food', 1299, 100, 1, 'food/c5d2b92e-c78f-4bf0-8504-154f033a8898.webp');
+INSERT INTO `products` (`id`, `name`, `category`, `price`, `discount_percentage`, `stock_quantity`, `is_active`, `img_url`) VALUES
+('875695ab-a760-486a-a801-9b052c8738b7', 'Ez egy teszt termék', 'drink', 1000, 0, 10, 1, 'drink/875695ab-a760-486a-a801-9b052c8738b7.webp'),
+('ac107756-f63c-4538-b4cc-91fc0cad6d82', 'Finom ital', 'drink', 2660, 0, 24, 1, 'drink/ac107756-f63c-4538-b4cc-91fc0cad6d82.webp'),
+('c5d2b92e-c78f-4bf0-8504-154f033a8898', 'Finom burger', 'food', 1299, 0, 100, 1, 'food/c5d2b92e-c78f-4bf0-8504-154f033a8898.webp');
 
 -- --------------------------------------------------------
 
@@ -160,15 +171,13 @@ INSERT INTO `products` (`id`, `name`, `category`, `price`, `stock_quantity`, `is
 CREATE TABLE `tables` (
   `id` char(36) NOT NULL,
   `number` int(11) NOT NULL,
-  `capacity` tinyint(4) NOT NULL
+  `capacity` tinyint(4) NOT NULL,
+  `is_booked` tinyint(1) NOT NULL,
+  `location_id` char(36) NOT NULL,
+  `booker_id` char(36) NOT NULL,
+  `booked_from` datetime NOT NULL,
+  `booked_to` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Dumping data for table `tables`
---
-
-INSERT INTO `tables` (`id`, `number`, `capacity`) VALUES
-('24639fdb-a474-486b-9432-4c7136ff0d10', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -245,7 +254,8 @@ ALTER TABLE `locations`
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `table_id` (`table_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `location_id` (`location_id`);
 
 --
 -- Indexes for table `order_items`
@@ -265,7 +275,9 @@ ALTER TABLE `products`
 -- Indexes for table `tables`
 --
 ALTER TABLE `tables`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location_id` (`location_id`),
+  ADD KEY `booker_id` (`booker_id`);
 
 --
 -- Indexes for table `table_guests`
@@ -309,7 +321,8 @@ ALTER TABLE `event_attendance`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`table_id`) REFERENCES `tables` (`id`),
-  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_4` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
 
 --
 -- Constraints for table `order_items`
@@ -317,6 +330,13 @@ ALTER TABLE `orders`
 ALTER TABLE `order_items`
   ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `tables`
+--
+ALTER TABLE `tables`
+  ADD CONSTRAINT `tables_ibfk_1` FOREIGN KEY (`booker_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `tables_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
 
 --
 -- Constraints for table `table_guests`

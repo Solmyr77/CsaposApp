d50406e06264ad2e9ecf7ed38f1c317caf38eed9
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2024-12-30 05:48:23 +0100

    Fixed minor issues

--- csaposapp.sql
+++ csaposapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2024 at 05:30 AM
+-- Generation Time: Dec 30, 2024 at 05:48 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 

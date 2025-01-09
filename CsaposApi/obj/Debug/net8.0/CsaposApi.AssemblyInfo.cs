91a009d43543a535805f123259e2a63b6e5b68a0
Solmyr77 <solmyr200@gmail.com>
2025-01-09 15:47:04 +0100

    Implemented dev access for db

--- CsaposApi/obj/Debug/net8.0/CsaposApi.AssemblyInfo.cs
+++ CsaposApi/obj/Debug/net8.0/CsaposApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb8057cb0fbfa56df005f39dca320621a993330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e78ad8806e76b19313fb553a8793a534094946")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

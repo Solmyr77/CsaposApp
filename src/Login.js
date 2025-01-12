c82a3a6eb7e1976f74828ee9fc5a56300d94a478
Agoston Attila <attilaagoston2005@gmail.com>
2025-01-12 21:40:23 +0100

    Added icons to login and register input fields

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Context from "./Context";
+import { UserCircleIcon, EyeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 
 function Login() {
   const navigate = useNavigate();
@@ -50,9 +51,15 @@ function Login() {
         <h1 className="font-bold text-3xl">Bejelentkezés</h1>
         <form className="flex flex-col mt-8 justify-evenly items-center" onSubmit={(event) => validateLogin(event)}>
             <label className="text-left w-full">Felhasználónév</label>
-            <input name="username" type="text" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required onChange={() => setErrorMessage("")}/>
+            <div className="relative mt-0.5 mb-4">
+              <input name="username" type="text" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required onChange={() => setErrorMessage("")}/>
+              <UserCircleIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+            </div>
             <label className="text-left w-full">Jelszó</label>
-            <input name="password" type="password" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required onChange={() => setErrorMessage("")}/>
+            <div className="relative mt-0.5 mb-4">
+              <input name="password" type="password" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required onChange={() => setErrorMessage("")}/>
+              <LockClosedIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+            </div>
             <p id="errorText" className={`text-center text-red-500 text-wrap max-w-40 ${errorMessage !== "" ? "visible" : "invisible"}`}>{errorMessage}</p>
             <button type="submit" className="w-full h-16 bg-blue rounded font-bold text-lg mt-4">Bejelentkezés</button>
         </form>
--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import BackButton from "./BackButton";
+import { AtSymbolIcon } from "@heroicons/react/20/solid";
+import { UserIcon, PencilIcon, EyeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 
 function Register() {
   const navigate = useNavigate();
@@ -76,17 +78,32 @@ function Register() {
         <h1 className="font-bold text-3xl">Regisztráció</h1>
         <form className="flex flex-col mt-8 justify-evenly items-center" onSubmit={(event) => validateForm(event)}>
           <label className="text-left w-full">Teljes név</label>
-          <input type="text" name="fullname" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required/>
+          <div className="relative mt-0.5 mb-4">
+            <input type="text" name="fullname" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required/>
+            <PencilIcon className="w-5 absolute top-1/2 right-2 -translate-y-1/2"/>
+          </div>
           <label className="text-left w-full">Születési idő</label>
           <input id="date" name="birthdate" type="date" className="bg-dark-grey px-5 rounded-md py-2 text-white mt-0.5 mb-4 w-full focus:outline-none" max={date.getFullYear()} required/>
           <label className="text-left w-full">Email cím</label>
-          <input type="email" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required/>
+          <div className="relative mt-0.5 mb-4">
+            <input type="email" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required/>
+            <AtSymbolIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+          </div>
           <label className="text-left w-full">Felhasználónév</label>
-          <input type="text" name="username" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required/>
+          <div className="relative mt-0.5 mb-4">
+            <input type="text" name="username" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required/>
+            <UserIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+          </div>
           <label className="text-left w-full">Jelszó</label>
-          <input type="password" id="password1" name="password1" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required onChange={() => setErrorMessage("")}/>
+          <div className="relative mt-0.5 mb-4">
+            <input type="password" id="password1" name="password1" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required onChange={() => setErrorMessage("")}/>
+            <LockClosedIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+          </div>
           <label className="text-left w-full">Jelszó újra</label>
-          <input type="password" name="password2" className="w-full bg-dark-grey px-5 py-2 rounded-md font-normal focus:outline-none mt-0.5 mb-4" required onChange={() => setErrorMessage("")}/>
+          <div className="relative mt-0.5 mb-4">
+            <input type="password" name="password2" className="w-full bg-dark-grey pl-5 pr-10 py-2 rounded-md font-normal focus:outline-none" required onChange={() => setErrorMessage("")}/>
+            <LockClosedIcon className="w-6 absolute top-1/2 right-2 -translate-y-1/2"/>
+          </div>
           <p id="errorText" className="text-center text-red-500 invisible text-wrap max-w-44" style={{"visibility" : `${errorMessage !== "" ? "visible" : "hidden"}`}}>{errorMessage}</p>
           <button type="submit" className="w-full h-16 bg-blue rounded font-bold text-lg mt-4">Regisztráció</button>
         </form>

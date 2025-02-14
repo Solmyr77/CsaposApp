6e75150e8b04e73f5f000b2809ac8235b1abed13
Solmyr77 <solmyr200@gmail.com>
2025-02-14 11:06:21 +0100

    Built solution

--- CsaposApi/obj/Debug/net8.0/CsaposApi.AssemblyInfo.cs
+++ CsaposApi/obj/Debug/net8.0/CsaposApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08beed2bcb10a6e79b54898fdff4ce4fc04bbd9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a9ef71026b5c869b77dee2b064169f45a8b3966")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsaposApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

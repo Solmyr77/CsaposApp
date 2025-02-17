dbd4d877878b22a97dfc0e49431210c3cc4334fa
Agoston Attila <attilaagoston2005@gmail.com>
2025-02-17 08:29:00 +0100

    Modified NotificationItem component design

--- src/NotificationItem.js
+++ src/NotificationItem.js
@@ -1,25 +1,17 @@
 import React, { useState, useContext, useEffect } from "react";
-import { EnvelopeIcon, CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon, CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Context from "./Context";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import getAccessToken from "./refreshToken";
 
 function NotificationItem({ record, isFriendRequest }) {
-  const { getProfile, friendRequests, setFriendRequests, setFriends, setPreviousRoutes, logout } = useContext(Context);
+  const { getProfile, setFriendRequests, setFriends, setPreviousRoutes, logout } = useContext(Context);
   const navigate = useNavigate();
   const location = useLocation();
   const [isAccepted, setIsAccepted] = useState(null);
-  const [Icon, setIcon] = useState(null);
   const [isRead, setIsRead] = useState(null);
   const [profile, setProfile] = useState({});
-
-  const importIcon = async() => {
-    if (isAccepted === true) {
-      return await import("@heroicons/react/20/solid/CheckCircleIcon");
-    }
-    return await import("@heroicons/react/20/solid/XCircleIcon");
-  }
 
   async function handleAccept(id) {
     try {
@@ -32,8 +24,9 @@ function NotificationItem({ record, isFriendRequest }) {
       const response = await axios.post(`https://backend.csaposapp.hu/api/friends/accept/${id}`, {}, config);
       if (response.status === 200) {
         setIsAccepted(true);
-        setTimeout(() => {
-          setFriends(state => state.filter(friend => friend.id !== profile.id));
+        setTimeout(async() => {
+          const friendProfile = await getProfile(record.userId1);
+          setFriends(state => [...state, friendProfile]);
           setFriendRequests(state => state.filter(friendRequest => friendRequest.id !== record.id));
         }, 2000);
       }
@@ -65,7 +58,6 @@ function NotificationItem({ record, isFriendRequest }) {
       if (response.status === 200) {
         setIsAccepted(false);
         setTimeout(() => {
-          setFriends(state => state.filter(friend => friend.id !== profile.id));
           setFriendRequests(state => state.filter(friendRequest => friendRequest.id !== record.id));
         }, 2000);
       }
@@ -87,8 +79,7 @@ function NotificationItem({ record, isFriendRequest }) {
   
   useEffect(() => {
     const run = async () => {
-      setIcon((await importIcon()).default);
-      if (isFriendRequest) {
+      if (record) {
         console.log(await getProfile(record.userId1));
         setProfile(await getProfile(record.userId1));
       }
@@ -98,21 +89,16 @@ function NotificationItem({ record, isFriendRequest }) {
 
   if (isFriendRequest === true) {
     return(
-      <div className="w-full h-16 bg-dark-grey rounded-md flex flex-row items-center p-4">
-        <img src={`https://assets.csaposapp.hu/assets/images/${profile.imageUrl}`} alt="" className="h-10 aspect-square rounded-full object-cover"/>
-        <div className="flex flex-row w-full pl-2 items-center">
-          <p className="flex flex-row items-center text-sm text-left text-nowrap basis-4/5"><span className="truncate inline-block max-w-20 mr-1 font-bold">{profile.displayName}</span> barátnak jelölt!</p>
-          {
-            isAccepted === null ? 
-            <div className="flex flex-row gap-x-1 basis-1/5">
-                <CheckCircleIcon className="h-10 text-green-500 hover:cursor-pointer" onClick={async () => await handleAccept(record.id)}/>
-                <XCircleIcon className="h-10 text-red-500 hover:cursor-pointer" onClick={async () => await handleReject(record.id)}/>
-            </div> :
-            <div className="flex flex-row gap-x-1 basis-1/5">
-                <Icon className={`h-10 text-green-500 ${isAccepted ? "visible" : "invisible"}`}/>
-                <Icon className={`h-10 text-red-500 ${isAccepted ? "invisible" : "visible"}`}/>
-            </div>  
-          }
+      <div className="w-full min-h-16 bg-dark-grey rounded-md flex flex-col p-4 pb-2">
+        <div className="flex flex-row basis-2/3 items-center">
+          <img src={`https://assets.csaposapp.hu/assets/images/${profile.imageUrl}`} alt="" className="h-10 aspect-square rounded-full object-cover mr-2"/>
+          <p className="flex flex-row items-center text-sm text-left text-nowrap basis-4/5"><span className="truncate inline-block max-w-20 mr-1 font-bold">{profile.displayName}asdfasdasdasd</span> barátnak jelölt!</p>
+        </div>
+        <div className="flex flex-row justify-end gap-2 basis-1/3 h-24 items-end">
+          <button className={`bg-green-500 px-4 py-1 rounded-md h-8 ${isAccepted !== null && "invisible"}`} onClick={() => handleAccept(record.id)}>Elfogadás</button>
+          <button className={`bg-red-500 px-4 py-1 rounded-md h-8 ${isAccepted !== null && "invisible"}`} onClick={() => handleReject(record.id)}>Elutasítás</button>
+          <button className={`bg-green-500 px-4 py-1 rounded-md h-8 ${isAccepted === true ? "flex" : "hidden"}`}>Elfogadva <CheckIcon/></button>
+          <button className={`bg-red-500 px-4 py-1 rounded-md h-8 ${isAccepted === false ? "flex" : "hidden"}`}>Elutasításva <XMarkIcon/></button>
         </div>
       </div>
     )
@@ -127,12 +113,12 @@ function NotificationItem({ record, isFriendRequest }) {
       navigate("/event");
       }}>
         <div className="relative">
-            <EnvelopeIcon className="h-10"/>
-            <div className={`absolute top-0.5 -right-[.125rem] rounded-full w-3 aspect-square bg-red-500 ${isRead ? "invisible" : "visible"}`}></div>
+          <EnvelopeIcon className="h-10"/>
+          <div className={`absolute top-0.5 -right-[.125rem] rounded-full w-3 aspect-square bg-red-500 ${isRead ? "invisible" : "visible"}`}></div>
         </div>
         <div className="flex flex-col w-full pl-4">
-            <p className="text-sm font-bold text-left">Azahriah koncert a Félidőben!</p>
-            <p className="w w line-clamp-1">A magyarországon világhírű énekes meghódítja a sajószögedi Félidő sörözőt.</p>
+          <p className="text-sm font-bold text-left">Azahriah koncert a Félidőben!</p>
+          <p className="w w line-clamp-1">A magyarországon világhírű énekes meghódítja a sajószögedi Félidő sörözőt.</p>
         </div>
     </div>
   );

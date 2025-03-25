dc15b699ede4a4e0755e9e012eb491f03b49573d
Agoston Attila <attilaagoston2005@gmail.com>
2025-03-25 09:20:36 +0100

    Fixed config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,16 +8,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        "grey": "#262626",
+        grey: "#262626",
         "dark-grey": "#1C1C1C",
-        "blue": "#007AFF",
-        "yellow": "#D7F205",
+        blue: "#007AFF",
+        yellow: "#D7F205",
         "dark-yellow": "#52591D",
-        "field": "#B2B2B2",
-        "field-selected": "#D5D5D5"
+        field: "#B2B2B2",
+        "field-selected": "#D5D5D5",
       },
       fontFamily: {
-        "play": ["Play", "sans-serif"]
+        play: ["Play", "sans-serif"],
       },
       fontSize: {
         xs: "0.75rem",
@@ -23,16 +25,14 @@ export default {
         md: "1.125rem",
         lg: "1.375rem",
         xl: "1.625rem",
-        xxl: "2rem"
+        xxl: "2rem",
       },
       screens: {
-        'xs': '480px',
-        '2xl': '1440px',
-        'sl': '900px',
+        xs: "480px",
+        "2xl": "1440px",
+        sl: "900px",
       },
     },
   },
-  plugins: [
-    require('daisyui')
-  ],
-}
+  plugins: [daisyui],
+};
